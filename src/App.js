f6991741b67961081d849a5a4475ea9d59793689
Susantakumarb24 <susantakumarb24@gmail.com>
2023-02-04 20:57:03 +0530

    n

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Susant
+          Learn React With Susant Kumar
         </a>
       </header>
     </div>
